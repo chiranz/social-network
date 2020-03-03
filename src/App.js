16f1c81ad13e336dfaf485d3f99ff645eb14ee90
chiranz <chiranpoudyal@gmail.com>
2020-03-03 13:10:53 +0530

    Landing Page Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./app.style.scss";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </Router>
   );
 }
